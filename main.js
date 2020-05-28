e443b2d88ec7e7a42d961302d6cb014cf86e2b34
Pg Gonni <p.gonni@hotmail.com>
2020-05-28 08:48:19 -0400

    Update to image element
    Added event listener to show and hide image UI, along with image UI animation. Added a (broken) loop over an array of images

---
+++ main.js
@@ -0,0 +1,24 @@
+// Create a function to trigger an event when the mouse stops moving
+(function (mouseStopDelay) {
+    var timeout;
+    document.addEventListener('mousemove', function (e) {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            var event = new CustomEvent("mousestop", {
+                detail: {
+                    clientX: e.clientX,
+                    clientY: e.clientY
+                },
+                bubbles: true,
+                cancelable: true
+            });
+            e.target.dispatchEvent(event);
+        }, mouseStopDelay);
+    });
+}(1500));
+
+const mouseLeaveUI = () => {
+    setTimeout(() => {
+        imgUIShow = false;
+    }, 1500);
+}
